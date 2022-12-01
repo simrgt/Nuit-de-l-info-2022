a370c4961324fee08f1b44d95c9099afcab71db6
Lesneo <64084644+simrgt@users.noreply.github.com>
2022-12-01 20:09:16 +0100

    konami code

--- site/js/codeKonami.js
+++ site/js/codeKonami.js
@@ -1,5 +1,14 @@
-$(init);
-
-function init (){
-
-}
+var k = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65],
+n = 0;
+$(document).keydown(function (e) {
+    if (e.keyCode === k[n++]) {
+        if (n === k.length) {
+            window.location.href = "./contact.html";
+            n = 0;
+            return false;
+        }
+    }
+    else {
+        n = 0;
+    }
+});
